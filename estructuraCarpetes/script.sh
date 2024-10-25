2cd89b26c2a2d398a9cd8074a7bad8dd5219bb7c
laasso <nrubialesguerrero@gmail.com>
2024-10-25 17:26:22 +0200

    estructura carpeta

---
+++ estructuraCarpetes/script.sh
@@ -0,0 +1,15 @@
+# Crear carpetas
+mkdir -p Empresa/Administració/Comptabilitat/Factures/{Clients/{2022/{01_Gener,02_Febrer},2023/{01_Gener,02_Febrer},2024},Proveïdors/{2022/{01_Gener,02_Febrer},2023/{01_Gener,02_Febrer},2024}}
+
+# Crear archivos de ejemplo en Clientes
+touch Empresa/Administració/Comptabilitat/Factures/Clients/2022/01_Gener/2022_01_ClientA.txt
+touch Empresa/Administració/Comptabilitat/Factures/Clients/2022/01_Gener/2022_01_ClientB.txt
+touch Empresa/Administració/Comptabilitat/Factures/Clients/2022/02_Febrer/2022_02_ClientC.txt
+touch Empresa/Administració/Comptabilitat/Factures/Clients/2022/02_Febrer/2022_02_ClientD.txt
+
+# Crear archivos de ejemplo en Proveïdors
+touch Empresa/Administració/Comptabilitat/Factures/Proveïdors/2022/01_Gener/2022_01_ProveidorX.txt
+touch Empresa/Administració/Comptabilitat/Factures/Proveïdors/2022/01_Gener/2022_01_ProveidorY.txt
+touch Empresa/Administració/Comptabilitat/Factures/Proveïdors/2022/02_Febrer/2022_02_ProveidorZ.txt
+touch Empresa/Administració/Comptabilitat/Factures/Proveïdors/2022/02_Febrer/2022_02_ProveidorW.txt
+
