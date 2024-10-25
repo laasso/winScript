bd2345bd330768bc52b716a64c0e4a9567bf3f62
laasso <nrubialesguerrero@gmail.com>
2024-10-25 16:35:49 +0200

    xcopy

--- copia.bat
+++ copia.bat
@@ -1,3 +1,3 @@
 @echo off
-mkdir C:\Users\isard\Desktop\copia 
-xcopy C:\Users\isard\Desktop C:\Users\isard\Desktop\copia
+mkdir C:\Users\isard\Escritorio\copia 
+xcopy C:\Users\isard\Escritorio C:\Users\isard\Escritorio\copia
